2ee50271fede2d770a5b4a75dcb36d6ba1d69cc7
Miklós Milosevits <milosevitsmiklos@gmail.com>
2022-11-18 13:57:53 +0100

    finish step 88

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,12 +1,52 @@
+const clientID = '58039086f34a41eb8e03de1ea119a085';
+const redirectURI = 'http://localhost:3000';
+
 let accessToken;
 
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
             return accessToken;
-        } else {
-            return;
         }
+
+        // check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}
+            `;
+            window.location = accessURL;
+        }
+    },
+    
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}
+        `, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artist[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        });
     }
 };
 
